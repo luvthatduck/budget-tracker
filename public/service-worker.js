83406552863b4b8f3d17a68212c20e28f0feca42
Janell Myrick <76677736+luvthatduck@users.noreply.github.com>
2021-06-03 22:27:22 -0600

    got the offline to work YAY

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,18 +1,21 @@
-const CACHE_NAME = 'budget-tracker-cache-v1';
-const DATA_CACHE_NAME = 'data-cache-v1';
+const APP_PREFIX  = 'budget-tracker-';
+const VERSION  = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
+const DATA_CACHE_NAME = "whatever";
 
 const FILES_TO_CACHE = [
-'/',
-'/index.html',
-'/css/styles.css',
-'/icons/icon-72x72.png',
-'/icons/icon-96x96.png',
-'/icons/icon-128x128.png ',
-'/icons/icon-144x144.png ',
-'/icons/icon-152x152.png',
-'/icons/icon-192x192.png ',
-'/icons/icon-384x384.png ',
-'/icons/icon-512x512.png',
+  "./",
+  "./index.html",
+  "./js/idb.js",
+  "./js/index.js",
+  "./css/styles.css",
+  "./icons/icon-72x72.png",
+  "./icons/icon-96x96.png",
+  "./icons/icon-128x128.png",
+  "./icons/icon-144x144.png",
+  "./icons/icon-192x192.png",
+  "./icons/icon-384x384.png",
+  "./icons/icon-512x512.png"
 ];
 
 // Install the service worker
